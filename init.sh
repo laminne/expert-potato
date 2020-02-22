849702a639b2164900e434a549f909b93cee6b66
laminne <gtattoda4@gmail.com>
2020-02-22 18:10:16 +0900

    create init.sh

---
+++ init.sh
@@ -0,0 +1,17 @@
+#! bin/bash
+echo "------------------------------------------------"
+echo  "Expert-Potato -Automation Package insraller"
+echo  "(C)laminne 2019-2020"
+echo "------------------------------------------------"
+echo .
+echo .
+echo Select installer number:
+echo e.g) 153
+read num
+
+echo Ok. Checking recipe server....
+wget https://clappdon.work/repos/amagasa/recipe/$num
+echo Checking Finished.
+echo Finish This Script....
+sh repo.sh
+exit 0
